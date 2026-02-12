e5f28387f4cc3c612d1a024362c199b31d7776e9
Jerome Fosse <jerome@fosse.nom.fr>
2026-02-12 09:55:09 +0100

    logs colorisées

--- app/lib/logger/logger.ts
+++ app/lib/logger/logger.ts
@@ -1,3 +1,5 @@
+import pc from "picocolors";
+
 export interface Logger {
     debug: (...args: unknown[]) => void
     info: (...args: unknown[]) => void
@@ -5,23 +7,41 @@ export interface Logger {
     error: (...args: unknown[]) => void
 }
 
+const isServer = typeof window === 'undefined';
+
 class ConsoleLogger implements Logger {
     debug(...args: unknown[]): void {
         if (process.env.NODE_ENV === 'development') {
-            console.log('%c[DEBUG]', 'color: #3B82F6; font-weight: bold', ...args);
+            if (isServer) {
+                console.log(pc.blue(pc.bold('[DEBUG]')), ...args);
+            } else {
+                console.log('%c[DEBUG]', 'color: #3B82F6; font-weight: bold', ...args);
+            }
         }
     }
 
     info(...args: unknown[]): void {
-        console.log('%c[INFO]', 'color: #11D128; font-weight: bold', ...args);
+        if (isServer) {
+            console.log(pc.green(pc.bold('[INFO]')), ...args);
+        } else {
+            console.log('%c[INFO]', 'color: #11D128; font-weight: bold', ...args);
+        }
     }
 
     warn(...args: unknown[]): void {
-        console.log('%c[WARN]', 'color: #F59E0B; font-weight: bold', ...args);
+        if (isServer) {
+            console.log(pc.yellow(pc.bold('[WARN]')), ...args);
+        } else {
+            console.log('%c[WARN]', 'color: #F59E0B; font-weight: bold', ...args);
+        }
     }
 
     error(...args: unknown[]): void {
-        console.log('%c[ERROR]', 'color: #CC0E0E; font-weight: bold', ...args);
+        if (isServer) {
+            console.error(pc.red(pc.bold('[ERROR]')), ...args);
+        } else {
+            console.log('%c[ERROR]', 'color: #CC0E0E; font-weight: bold', ...args);
+        }
     }
 }
 
